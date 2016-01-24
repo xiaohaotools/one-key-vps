83d768209e2b957826de3f2a21810701637cb4a5
朱延龙 <zhuyanlong@live.cn>
2016-01-24 23:24:13 +0800

    test .sh file

---
+++ my-tmp.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# Get IP address of the server
+function get_my_ip(){
+    echo "Preparing, Please wait a moment..."
+    IP=`curl -s checkip.dyndns.com | cut -d' ' -f 6  | cut -d'<' -f 1`
+    if [ -z $IP ]; then
+        IP=`curl -s ifconfig.me/ip`
+    fi
+}
+
+get_my_ip
