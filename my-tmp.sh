a9a70d4093a57b95b9a1d5fe6be80be62a40e8b3
朱延龙 <zhuyanlong@live.cn>
2016-01-30 00:35:19 +0800

    fix

--- my-tmp.sh
+++ my-tmp.sh
@@ -24,6 +24,7 @@ function install_SS(){
     pre_doc
     apps
     show_doc
+    optimizatioin
     run_doc
 }
 # Make sure only root can run our script
@@ -82,6 +83,40 @@ function apps(){
     chmod a+rx speedtest_cli.py
     sudo mv speedtest_cli.py /usr/local/bin/speedtest-cli
     sudo chown root:root /usr/local/bin/speedtest-cli
+}
+
+function optimizatioin(){
+echo "
+* soft nofile 51200
+* hard nofile 51200
+" > /etc/security/limits.conf
+echo "
+session required pam_limits.so
+" > /etc/pam.d/common-session
+echo "
+ulimit -SHn 51200
+" > /etc/profile
+echo "
+net.ipv4.ip_forward = 1
+fs.file-max = 51200
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_tw_recycle = 0
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_keepalive_time = 1200
+net.ipv4.ip_local_port_range = 10000 65000
+net.ipv4.tcp_max_syn_backlog = 8192
+net.ipv4.tcp_max_tw_buckets = 5000
+net.ipv4.tcp_fastopen = 3
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+net.ipv4.tcp_rmem = 4096 87380 67108864
+net.ipv4.tcp_wmem = 4096 65536 67108864
+net.core.netdev_max_backlog = 250000
+net.ipv4.tcp_mtu_probing = 1
+" > /etc/sysctl.conf
+sysctl -p
+
 }
 
 install_SS
