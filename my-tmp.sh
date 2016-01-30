bda86b00958ecdcfd8c6be93dd0556008d621075
朱延龙 <zhuyanlong@live.cn>
2016-01-30 11:23:47 +0800

    fix

--- my-tmp.sh
+++ my-tmp.sh
@@ -75,8 +75,10 @@ function show_doc(){
 }
 
 function run_doc(){
-    ssserver -c /etc/shadowsocks.json -d start
+    echo "reboot"
+    echo "ssserver -c /etc/shadowsocks.json -d start"
     echo "Test your VPS: speedtest-cli"
+    echo "/sbin/modprobe tcp_hybla"
 }
 function apps(){
     wget https://raw.github.com/sivel/speedtest-cli/master/speedtest_cli.py
@@ -89,13 +91,13 @@ function optimizatioin(){
 echo "
 * soft nofile 51200
 * hard nofile 51200
-" > /etc/security/limits.conf
+" >> /etc/security/limits.conf
 echo "
 session required pam_limits.so
-" > /etc/pam.d/common-session
+" >> /etc/pam.d/common-session
 echo "
 ulimit -SHn 51200
-" > /etc/profile
+" >> /etc/profile
 echo "
 net.ipv4.ip_forward = 1
 fs.file-max = 51200
@@ -107,14 +109,15 @@ net.ipv4.tcp_keepalive_time = 1200
 net.ipv4.ip_local_port_range = 10000 65000
 net.ipv4.tcp_max_syn_backlog = 8192
 net.ipv4.tcp_max_tw_buckets = 5000
+net.ipv4.tcp_fastopen = 3
 net.core.rmem_max = 67108864
 net.core.wmem_max = 67108864
 net.ipv4.tcp_rmem = 4096 87380 67108864
 net.ipv4.tcp_wmem = 4096 65536 67108864
 net.core.netdev_max_backlog = 250000
-net.ipv4.tcp_mtu_probing=1
-net.ipv4.tcp_congestion_control=hybla
-" > /etc/sysctl.conf
+net.ipv4.tcp_mtu_probing = 1
+net.ipv4.tcp_congestion_control = hybla
+" >> /etc/sysctl.conf
 sysctl -p
 /sbin/modprobe tcp_hybla
 }
